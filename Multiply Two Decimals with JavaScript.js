dd8670abf670f9f91a3bc4d7a151a4032ec0763b
Ashwani Kumar Sonu <94763917+AshwaniKrSonu619@users.noreply.github.com>
2022-08-04 02:16:45 +0530

    mul two decl

---
+++ Multiply Two Decimals with JavaScript.js
@@ -0,0 +1,17 @@
+// In JavaScript, you can also perform calculations with decimal numbers, just like whole numbers.
+
+// Let's multiply two decimals together to get their product.
+
+// Change the 0.0 so that product will equal 5.0.
+
+// Tests
+// Waiting:The variable product should equal 5.0.
+// Waiting:You should use the * operator
+
+
+
+
+const product = 2.0 * 2.5;
+console.log(product)
+
+
