9e1b9d45bf1308a5e046e1f11cf7ead4ba9e7f20
Ashwani Kumar Sonu <94763917+AshwaniKrSonu619@users.noreply.github.com>
2022-08-04 01:05:18 +0530

    Diff b/w the var and let Keywords

---
+++ Hello Javascript.js
@@ -0,0 +1,66 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+console.log("Welcome to Javascript!");
+
+
+
+var camper = "James";
+var camper = "David";
+console.log(camper);
+
+// Setup
+var a;
+a = 7;
+var b;
+var b = 7;
+var b = a;
+
+// Only change code below this line
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+var camper = "James";
+var camper = "David";
+console.log(camper);
+console.log(a)
+console.log(b)
+console.log(studlyCapVar)
+console.log(properCamelCase)
+console.log(titleCaseOver)
+
+
+
+//  (Question)s One of the biggest problems with declaring variables with the var keyword is that you can easily overwrite variable declarations:
+
+//  var camper = "James";
+//  var camper = "David";
+//  console.log(camper);
+//  In the code above, the camper variable is originally declared as James, and is then overridden to be David. The console then displays the string David.
+
+//  In a small application, you might not run into this type of problem. But as your codebase becomes larger, you might accidentally overwrite a variable that you did not intend to. Because this behavior does not throw an error, searching for and fixing bugs becomes more difficult.
+
+//  A keyword called let was introduced in ES6, a major update to JavaScript, to solve this potential issue with the var keyword. You'll learn about other ES6 features in later challenges.
+
+//  If you replace var with let in the code above, it results in an error:
+
+//  let camper = "James";
+//  let camper = "David";
+//  The error can be seen in your browser console.
+
+//  So unlike var, when you use let, a variable with the same name can only be declared once.
+
+//  Update the code so it only uses the let keyword.
+
+
+
+let catName = "Oliver";
+let catSound = "Meow!";
+console.log(catSound);
+
